68671317c125048c504bd8d57b49cd9c4f7bfe7a
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-18 19:47:31 -0600

    1st commit - selecting datatypes.

---
+++ answers.js
@@ -0,0 +1,29 @@
+// 1. If else statement
+lightSwitch === true;
+
+if (lightSwitch === true) {
+	console.log("The light is on!");
+} else {
+	console.log("The light is off.");
+};
+
+// 2. String
+console.log("eterictodd@gmail.com");
+
+// 3. Object
+const spaceShip = {
+	name: "Pillar of Autumn",
+	hullIntegrity: 100,
+	cannons: 6,
+	tractorBeam: "Inactive",
+	warpDrive: "Ready"
+};
+
+// 4. Array
+const classNames = ["student1", "student2", "student3"];
+
+// 5. Nested array
+const classLocations = [["student1", "location1"], ["student2", "location2"], ["student3", "location3"]];
+
+// 6. Nested array
+const classFavoriteTvShows = [["student1", "location1", "tvShow1"], ["student2", "location2", "tvShow2"], ["student3", "location3", "tvShow3"]];
