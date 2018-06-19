53986f1e85a246017411ad963fd8c120ee7862cb
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-18 20:17:53 -0600

    2nd commit - created some data structures and accessed some data.

---
+++ js/answers.js
@@ -0,0 +1,72 @@
+console.log("JavaScript is running...");
+
+// Refresher
+
+// 1. If else statement
+let lightSwitch = true;
+
+if (lightSwitch === true) {
+	console.log("The light is on!");
+} else {
+	console.log("The light is off.");
+};
+
+// 2. String
+console.log("eterictodd@gmail.com");
+
+// 3. Object
+const spaceShip = {
+	name: "Pillar of Autumn",
+	hullIntegrity: 100,
+	cannons: 6,
+	tractorBeam: "Inactive",
+	warpDrive: "Ready"
+};
+
+// 4. Array
+const classNames = ["student1", "student2", "student3"];
+
+// 5. Nested array
+const classLocations = [["student1", "location1"], ["student2", "location2"], ["student3", "location3"]];
+
+// 6. Nested array
+const classFavoriteTvShows = [["student1", "location1", "tvShow1"], ["student2", "location2", "tvShow2"], ["student3", "location3", "tvShow3"]];
+
+
+// Take it easy
+
+// 1. Rainbow array
+const rainbow = ["red", "blue", "green", "yellow", "purple", "orange"];
+
+// 2. Accessing blue
+console.log(rainbow[1]);
+
+// 3. The me object
+const eric = {
+  favoriteFood: "Chicken tikka masala",
+  hobby: "Video Games",
+  currentResidence: "Glendale",
+  dataType: "Objects"
+};
+
+console.log(eric.hobby);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
